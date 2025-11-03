24ebc2253912d6c5ae53b894af57c99fa1be411e
Casey Waweru <casey.waweru@gmail.com>
2025-11-03 21:38:01 +0300

    Adding components folder, with BotCollection, Header and MyArmy Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return(
+    <>
+      <div className="bg-gray-200 rounded flex justify-center h-6 items-center m-2">
+        <h1>
+          Bot Battlr
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/MyBotArmy.jsx
@@ -0,0 +1,7 @@
+// Personal botarmy
+
+// uses state to hold the value of my army
+function MyBotArmy() {
+}
+
+export default MyBotArmy;
