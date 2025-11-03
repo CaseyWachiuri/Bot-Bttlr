d0752dc95448a8b6a6b287376ab68370346e9cf6
Casey Waweru <casey.waweru@gmail.com>
2025-11-03 22:41:09 +0300

    Functionality to render bots with seperated card

---
+++ src/components/BotCard.jsx
@@ -0,0 +1,27 @@
+function BotCard({ bot }) {
+
+  // Destructuring the bot data
+  const {id, name, health, damage, armor, bot_class, catchphrase, avatar_url} = bot;
+  console.log("what is going on");
+
+  return (
+    <>
+      {/* Styling for card */}
+      <div key={id} className="ml-2 mr-2 mt-2 max-w-3xs rounded overflow-hidden shadow-lg" > {/* Add onClick handler */}
+        <img className="w-full" src={avatar_url} alt="Example botcard. Image goes here" />
+        <div className="px-6 py-4 ">
+          <div className="font-bold text-sm mb-2 ">{ name }</div>
+          <p className="text-gray-700 text-xs">{ catchphrase }</p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">{ health }</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">{ damage }</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">{ armor }</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">{ bot_class }</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BotCard;
--- src/components/BotCollection.jsx
+++ src/components/BotCollection.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import BotCard from "./BotCard";
 
 function BotCollection() {
   const [bots, setBots] = useState([]);
@@ -14,21 +15,15 @@ function BotCollection() {
     fetchData();
   },[])
 
+  // Iteration through the array to create each bot card element
+  const botList = bots.map((item) => <BotCard bot={item} />);
+
+  if (!botList) return <div>Loading BotList...</div>
+
   return (
     <>
-      {/* Styling for card */}
-      <div className="ml-2 mt-2 max-w-3xs rounded overflow-hidden shadow-lg" > {/* Add onClick handler */}
-        <img className="w-full" src="null" alt="Example botcard. Image goes here" />
-        <div className="px-6 py-4 ">
-          <div className="font-bold text-sm mb-2 ">BotName</div>
-          <p className="text-gray-700 text-xs">Bot Catch Phrase</p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Health</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Damage</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Armor</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Bot Class</span>
-        </div>
+      <div className="flex flex-wrap justify-between ">
+        { botList }
       </div>
     </>
   );
