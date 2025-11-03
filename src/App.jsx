ae7b8239ed9d595c8854c1534393b2fb1d3cf5c3
Casey Waweru <casey.waweru@gmail.com>
2025-11-03 21:38:49 +0300

    Moving code to respective components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { useState } from 'react'
+import Header from "./components/Header";
+import BotCollection from "./components/BotCollection";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* Styling for card */}
-      <div className="ml-2 mt-2 max-w-3xs rounded overflow-hidden shadow-lg">
-        <img className="w-full" src="null" alt="Example botcard. Image goes here" />
-        <div className="px-6 py-4 ">
-          <div className="font-bold text-sm mb-2 ">BotName</div>
-          <p className="text-gray-700 text-xs">Bot Catch Phrase</p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Health</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Damage</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Armor</span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-1 mb-2">Bot Class</span>
-        </div>
-      </div>
+      <Header />
+      <BotCollection />
     </>
   )
 }
